d6be75e416d37b2f1025ea93734c246a9ced9162
Carlos <carlos.andre@voxtecnologia.com.br>
2021-10-18 11:49:37 -0300

    Implementing Sentry logger

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -2,6 +2,9 @@ import express from 'express';
 import 'express-async-errors';
 import 'reflect-metadata';
 
+import * as Sentry from "@sentry/node";
+import * as Tracing from "@sentry/tracing";
+
 import routes from '@shared/infra/http/routes';
 import exceptionHandler from '@shared/infra/http/middlewares/exceptionHandler';
 
@@ -10,9 +13,23 @@ import '@shared/container';
 
 const app = express();
 
+Sentry.init({
+  dsn: "https://8633986b2c324cbf90e43f9f70943ade@o1043018.ingest.sentry.io/6012304",
+  integrations: [
+    new Sentry.Integrations.Http({ tracing: true }),
+    new Tracing.Integrations.Express({ app }),
+  ],
+  tracesSampleRate: 1.0,
+});
+
+app.use(Sentry.Handlers.requestHandler());
+app.use(Sentry.Handlers.tracingHandler());
+
 app.use(express.json());
 
 app.use(routes);
+
+app.use(Sentry.Handlers.errorHandler());
 
 app.use(exceptionHandler);
 
